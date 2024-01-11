6524da680ce3362acf576152344c73e647b41d6a
Romero Cristian Martin <crisromero@andreani.com>
2024-01-11 13:24:43 -0300

    front done

--- frontend-mvp/src/App.tsx
+++ frontend-mvp/src/App.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
+  
+  const [names,setNames] = React.useState(['Pepito Grillo','Santiago Illi','Blenza'])
+  const [newUser,setNewUser] = React.useState('')
+  const [data, setData] = React.useState('start api')
+
+  function handleChange (event:any) {
+    setNewUser(event.target.value)
+  }
+
+  function addUser() {
+    setNames(prevNamesArray => [...prevNamesArray,newUser])
+  }
+
+  React.useEffect(() => {
+    // Make a GET request to the server
+    fetch('http://localhost:4000')
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData.message);
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <label className="userInputLine">Ingrese nombre de Usuario: 
+        <input 
+          type='text' 
+          placeholder='Ingrese nombre'
+          onChange={handleChange}>
+        </input>
+      </label>
+      <Button className="button" variant="contained" onClick={addUser}>Ingresar</Button>
+      <h3 className='list'>Lista de usuarios:</h3>
+      <ul>
+        {names.map(name=><li>{name}</li>)}
+      </ul>
+      <p>{data}</p>
     </div>
+    
   );
 }
 
